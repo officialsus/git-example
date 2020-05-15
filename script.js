6dc1f0f1f030a6a9da4b7a5e10664b1f9387c723
Jacob Colmenero <jacob@officialsus.com>
2020-05-15 04:12:42 -0700

    update

--- script.js
+++ script.js
@@ -2,15 +2,32 @@ function idCard() {
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
     var address = document.getElementById("address").value;
-    var age = document.getElementById("age").value;
-    var phoneNumber = document.getElementById("phoneNumber").value;
+    var age = parseInt(document.getElementById('age').value);
+    var phoneNumber = parseInt(document.getElementById('age').value);
     var fullName = firstName + " " + lastName;
     document.getElementById("postFullName").innerHTML = fullName;
     document.getElementById("postAddress").innerHTML = address;
+    numberArray = [];
+    numberArray.push(age+phoneNumber);
 
+        for (var i = 0; i<numberArray.length; i++){
+            if (numberArray[i] <= 100) {
+            document.getElementById("postAge").innerHTML = "age:" + age;    
+            }
+            else if (numberArray[i] > 100) {
+            document.getElementById("postPhoneNumber").innerHTML = "Phone Number:" + phoneNumber;
+            }
+        }
 }
 
-numberArray
+
+
+
+
+
+console.log(numberArray);
+
+
 
 
 
