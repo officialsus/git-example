b3742617f46153a409e57c766b717fb57961bbf8
Jacob Colmenero <jacob@officialsus.com>
2020-05-14 23:24:38 -0700

    First Draft

---
+++ script.js
@@ -0,0 +1,20 @@
+function idCard() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var address = document.getElementById("address").value;
+    var age = document.getElementById("age").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var fullName = firstName + " " + lastName;
+    document.getElementById("postFullName").innerHTML = fullName;
+    document.getElementById("postAddress").innerHTML = address;
+
+}
+
+numberArray
+
+
+
+
+
+
+
